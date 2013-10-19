36043381d0b5a52b1b1c2a55b6f2393f12aa98a7
DrWhat <drwhat@cryto.net>
2013-10-19 20:44:30 +0100

    Post, retrieve. not working condition

--- src/class/paste.class.php
+++ src/class/paste.class.php
@@ -16,16 +16,17 @@ class submit_paste {
 		global $database;
 		global $member;
 		$sections = explode(":",$this->crypt_paste($paste));
-		//echo "Key: ".$sections[0]." </br>Encryted text: ".$sections[1]." </br>IV: ".$sections[2];
+		echo "Key: ".$sections[0]."</br>Encryted text: ".$sections[1]."</br>IV: ".$sections[2];
 
 		$database->query("INSERT INTO pastes (title, userid, paste, iv) VALUES (:title, :userid, :paste, :iv);",
 			array(':title' => $title, ':userid' => $userid, ':paste' => $sections[1], ':iv' => $sections[2]));
 
-		$database->query("SELECT id, iv, key FROM pastes WHERE title = :title AND iv = :iv LIMIT 0, 1;",array(':title' => $title, ':iv' => $sections[2]));
-		if($database->count() == 1) {
+		$database->query("SELECT id FROM pastes WHERE title = :title AND iv = :iv LIMIT 0, 1;",
+			array(':title' => $title, ':iv' => $sections[2]));
+		if($database->count() === 1) {
 			$data = $database->statement->fetch(PDO::FETCH_OBJ);
-			echo "You can view your paste with the following link</br>
-				<a href=\"".$member->currentPath()."paste.php?action=getpaste&iv=".$data->iv."&id=".$data->id."&key=".$data->key."\"/>Here</a></br>";
+			echo "</br>You can view your paste with the following link</br>
+				<a href=\"".$member->currentPath()."paste.php?action=getpaste&id=".$data->id."&iv=".$sections[2]."&key=".$sections[0]."\"/>Here</a></br>";
 		} else echo "</br>Database error</br>";
 		return;
 	}
@@ -44,16 +45,16 @@ class submit_paste {
 		global $database;
 		$database->query("SELECT paste, title FROM pastes WHERE iv = :iv AND id = :id LIMIT 0, 1;",
 			array(':iv' => $iv, ':id' => $pasteid));
-		if($database->count() >= '1') {
+
+		if($database->count() === 1) {
 			$data = $database->statement->fetch(PDO::FETCH_OBJ);
-			//echo $data->paste."</br>";
-			if (null == $key) {
+			echo $data->paste."</br>";
+			if (null === $key) {
 				echo "</br>You did no supply a key, Decryption was not attempted.";
 			} else {
 				$d = $this->decrypt_paste($data->paste,$key,$iv);
 				echo "</br>title: ".$data->title. "</br>text: ".$d;
 			}
-
 			return;
 		} else {
 			$err = "E1";
--- src/paste.php
+++ src/paste.php
@@ -6,32 +6,36 @@ if(isset($_GET['action'])) {
 	$action = null;
 	$input = "This is an interface to the mcrypt library, which supports a wide variety of block algorithms such as DES, TripleDES, Blowfish (default), 3-WAY, SAFER-SK64, SAFER-SK128, TWOFISH, TEA, RC2 and GOST in CBC, OFB, CFB and ECB cipher modes. Additionally, it supports RC6 and IDEA which are considered non-free. CFB/OFB are 8bit by default. ";// text to encrypt
 	//$submit->submitpaste("some titles", $input);
-	if ($submit->submitpaste("mCrypt paste",$input)==="E10"){
-		echo "You are not logged in";
-	}
+	//if ($submit->submitpaste("mCrypt paste",$input)==="E10") echo "You are not logged in";
 }
 if ($action == 'submit') {
 	$title = (isset($_POST['title']))?$_POST['title']:'';
 	$data = (isset($_POST['paste']))?$_POST['paste']:'E99_ND';
-	if($data === 'E99_ND') echo "No data was submited"; else $submit->submitpaste($title,$data);
+	if($data === 'E99_ND') echo "No data was submited";
+	else $submit->submitpaste($title,$data);
 	echo $title,$data;
 } elseif ($action == 'getpaste'){
 	$iv = (isset($_GET['iv']))?$_GET['iv']:'';
 	$id = (isset($_GET['id']))?$_GET['id']:'';
 	$key = (isset($_GET['key']))?$_GET['key']:null;
 	$key = str_replace(" ", "+", $key);
-	echo "1</br>";
-	if (null != @$key){
-		echo "3</br>";
-		echo $iv."</br>".$id."</br>".$key;
-		if ($submit->grabpaste($id,$iv,$key) === "E10"){
+	if (null === @$key){
+		$paste = $submit->grabpaste($id,$iv);
+		echo "Does not have key";
+		if ($paste === "E10") {
 			echo "You are not logged in";
+		} elseif ($paste === "E1") {
+			echo "Database error";
 		}
+
 	} else {
-		echo "2</br>";
-		echo $iv."</br>".$id;
-		if ($submit->grabpaste($id,$iv) === "E10"){
+
+		$paste = $submit->grabpaste($id,$iv,$key);
+		echo "has key";
+		if ($paste === "E10") {
 			echo "You are not logged in";
+		} elseif ($paste === "E1") {
+			echo "Database error";
 		}
 	}
 }
